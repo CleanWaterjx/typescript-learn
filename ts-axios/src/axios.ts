14cebe70c2e305331c6d38fbbbdaff165b4ce24c
cleanwaterjx <316756384@qq.com>
2021-07-22 00:30:30 +0800

    feat: 扩展get等接口

--- ts-axios/src/axios.ts
+++ ts-axios/src/axios.ts
@@ -1,39 +1,16 @@
-import { AxiosPromise, AxiosRequestConfig, AxiosResponse } from './types'
-import xhr from './xhr'
-import { buildURL } from './helpers/url'
-import { transformRequest, transformResponse } from './helpers/data'
-import { processHeaders } from './helpers/headers'
+import Axios from "./core/Axios";
+import { extend } from "./helpers/util";
+import { AxisoInstance } from "./types";
 
-function axios(config: AxiosRequestConfig): AxiosPromise {
-  processConfig(config)
-  return xhr(config).then((res) =>{
-    return transformResponseData(res)
-  })
+function createInstance(): AxisoInstance {
+    const context = new Axios()
+    const instance = Axios.prototype.request.bind(context)
+
+    extend(instance, context)
+
+    return instance as AxisoInstance
 }
 
-function processConfig(config: AxiosRequestConfig): void {
-  config.url = transformURL(config)
-  config.headers = transformHeaders(config)
-  config.data = transformRequestData(config)
-}
+const axios = createInstance()
 
-function transformURL(config: AxiosRequestConfig): string {
-  const { url, params } = config
-  return buildURL(url, params)
-}
-
-function transformRequestData(config: AxiosRequestConfig): any {
-  return transformRequest(config.data)
-}
-function transformHeaders(config: AxiosRequestConfig): any {
-  const { headers = {}, data } = config
-  return processHeaders(headers,data)
-}
-
-function transformResponseData(res: AxiosResponse): AxiosResponse {
-  
-  res.data  = transformResponse(res.data)
-
-  return res
-}
 export default axios
--- ts-axios/src/types/index.ts
+++ ts-axios/src/types/index.ts
@@ -1,12 +1,15 @@
+import axios from "../axios"
+
 export type Method = 'get' | 'GET'
     | 'delete' | 'DELETE'
     | 'head' | 'HEAD'
     | 'options' | 'OPTIONS'
     | 'post' | 'POST'
     | 'patch' | 'PATCH'
+    | 'put' | 'PUT'
 
 export interface AxiosRequestConfig {
-    url: string
+    url?: string
     method?: Method
     data?: any
     params?: any
@@ -33,4 +36,27 @@ export interface AxisoError extends Error {
     code?: string | null
     request?: any
     response?: AxiosResponse
+}
+
+export interface Axios {
+    request(config: AxiosRequestConfig): AxiosPromise
+
+    get(url: string, config?: AxiosRequestConfig): AxiosPromise
+    
+    delete(url: string, config?: AxiosRequestConfig): AxiosPromise
+    
+    head(url: string, config?: AxiosRequestConfig): AxiosPromise
+    
+    options(url: string, config?: AxiosRequestConfig): AxiosPromise
+
+    post(url: string, data?: any, config?: AxiosRequestConfig): AxiosPromise
+    
+    put(url: string, data?: any, config?: AxiosRequestConfig): AxiosPromise
+
+    patch(url: string, data?: any, config?: AxiosRequestConfig): AxiosPromise
+
+}
+
+export interface AxisoInstance extends Axios{
+    (config: AxiosRequestConfig): AxiosPromise
 }
