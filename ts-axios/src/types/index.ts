e7cc7725249eab92d76a6df3c2c601da5e893901
jinxiong yu <jinxiongyu@jinxiongdeMacBook-Pro.local>
2021-07-26 22:14:51 +0800

    feat: axios 函数重载

--- ts-axios/src/types/index.ts
+++ ts-axios/src/types/index.ts
@@ -1,62 +1,70 @@
-import axios from "../axios"
+import axios from '../axios'
 
-export type Method = 'get' | 'GET'
-    | 'delete' | 'DELETE'
-    | 'head' | 'HEAD'
-    | 'options' | 'OPTIONS'
-    | 'post' | 'POST'
-    | 'patch' | 'PATCH'
-    | 'put' | 'PUT'
+export type Method =
+  | 'get'
+  | 'GET'
+  | 'delete'
+  | 'DELETE'
+  | 'head'
+  | 'HEAD'
+  | 'options'
+  | 'OPTIONS'
+  | 'post'
+  | 'POST'
+  | 'patch'
+  | 'PATCH'
+  | 'put'
+  | 'PUT'
 
 export interface AxiosRequestConfig {
-    url?: string
-    method?: Method
-    data?: any
-    params?: any
-    headers?: any
-    responseType?: XMLHttpRequestResponseType
-    timeout?: number
+  url?: string
+  method?: Method
+  data?: any
+  params?: any
+  headers?: any
+  responseType?: XMLHttpRequestResponseType
+  timeout?: number
 }
 
 export interface AxiosResponse {
-    data: any
-    status: number
-    statusText: string
-    headers: any
-    config: AxiosRequestConfig
-    request: any
+  data: any
+  status: number
+  statusText: string
+  headers: any
+  config: AxiosRequestConfig
+  request: any
 }
 
-export interface AxiosPromise extends Promise<AxiosResponse>{
-}
+export interface AxiosPromise extends Promise<AxiosResponse> {}
 
 export interface AxisoError extends Error {
-    isAxiosError: boolean
-    config: AxiosRequestConfig
-    code?: string | null
-    request?: any
-    response?: AxiosResponse
+  isAxiosError: boolean
+  config: AxiosRequestConfig
+  code?: string | null
+  request?: any
+  response?: AxiosResponse
 }
 
 export interface Axios {
-    request(config: AxiosRequestConfig): AxiosPromise
+  request(config: AxiosRequestConfig): AxiosPromise
 
-    get(url: string, config?: AxiosRequestConfig): AxiosPromise
-    
-    delete(url: string, config?: AxiosRequestConfig): AxiosPromise
-    
-    head(url: string, config?: AxiosRequestConfig): AxiosPromise
-    
-    options(url: string, config?: AxiosRequestConfig): AxiosPromise
+  get(url: string, config?: AxiosRequestConfig): AxiosPromise
 
-    post(url: string, data?: any, config?: AxiosRequestConfig): AxiosPromise
-    
-    put(url: string, data?: any, config?: AxiosRequestConfig): AxiosPromise
+  delete(url: string, config?: AxiosRequestConfig): AxiosPromise
 
-    patch(url: string, data?: any, config?: AxiosRequestConfig): AxiosPromise
+  head(url: string, config?: AxiosRequestConfig): AxiosPromise
 
+  options(url: string, config?: AxiosRequestConfig): AxiosPromise
+
+  post(url: string, data?: any, config?: AxiosRequestConfig): AxiosPromise
+
+  put(url: string, data?: any, config?: AxiosRequestConfig): AxiosPromise
+
+  patch(url: string, data?: any, config?: AxiosRequestConfig): AxiosPromise
 }
 
-export interface AxisoInstance extends Axios{
-    (config: AxiosRequestConfig): AxiosPromise
+export interface AxisoInstance extends Axios {
+  (config: AxiosRequestConfig): AxiosPromise
+
+  (url: string, config?: AxiosRequestConfig): AxiosPromise
 }
