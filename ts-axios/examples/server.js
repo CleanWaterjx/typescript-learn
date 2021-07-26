5cbcd1b41824f3b6f2da9d01c7b79ce0a5dbeb7a
jinxiong yu <jinxiongyu@jinxiongdeMacBook-Pro.local>
2021-07-26 23:04:25 +0800

    feat: 响应数据支持范型

--- ts-axios/examples/server.js
+++ ts-axios/examples/server.js
@@ -123,5 +123,16 @@ function registerExtendRouter(params) {
   router.patch('/extend/patch', function (req, res) {
     res.json(req.body)
   })
+
+  router.get('/extend/user', function (req, res) {
+    res.json({
+      code: 0,
+      message: 'ok',
+      result: {
+        name: 'jack',
+        age: 18
+      }
+    })
+  })
 }
 app.use(router)
