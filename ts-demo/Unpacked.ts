c75b4e9c687f6d9f31bde52cbc704dc91bfaacbd
余锦雄 <316756384@qq.com>
2023-04-14 10:56:27 +0800

    feat: 解压抽离关键类型

---
+++ ts-demo/Unpacked.ts
@@ -0,0 +1,13 @@
+// 解压抽离关键类型 https://juejin.cn/post/6844903860692074504#heading-17
+type Unpacked<T> =
+    T extends (infer U)[] ? U :
+    T extends (...args: any[]) => infer U ? U :
+    T extends Promise<infer U> ? U :
+    T;
+
+type T0 = Unpacked<string>;  // string
+type T1 = Unpacked<string[]>;  // string
+type T2 = Unpacked<() => string>;  // string
+type T3 = Unpacked<Promise<string>>;  // string
+type T4 = Unpacked<Promise<string>[]>;  // Promise<string>
+type T5 = Unpacked<Unpacked<Promise<string>[]>>;  // string
